82c9c9ec5cacc567adbea1e9427f005029635a31
Rodrigo De Frutos <darkrodry@gmail.com>
2017-08-25 16:25:04 +0200

    Exercise 3.19

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -116,4 +116,7 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def map[A,B](l: List[A])(f: A => B): List[B] =
     foldRight(l, List[B]())((a, b) => Cons(f(a), b))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldRight(as, List[A]())((a, b) => if (f(a)) Cons(a, b) else b)
 }
